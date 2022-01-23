b73be3c7cd8d1438050c53f4abc9f2894f3ef2f6
nxbeyxnd <unblocksteam@gmail.com>
2022-01-23 15:19:38 +0300

    Init

---
+++ src/main/java/ru/teamandersen/AndersenTeamJavaApplication.java
@@ -0,0 +1,13 @@
+package ru.teamandersen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndersenTeamJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndersenTeamJavaApplication.class, args);
+    }
+
+}
