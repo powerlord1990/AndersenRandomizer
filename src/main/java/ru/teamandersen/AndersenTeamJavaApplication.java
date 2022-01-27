adacb311cad16fa2cd3957ae7f212708ef9ab5cb
sanonttame <amoyst.notna@gmail.com>
2022-01-27 19:17:26 +0300

    add maven dependency apache poi 4.1.2 wth ooxml add readExel()

--- src/main/java/ru/teamandersen/AndersenTeamJavaApplication.java
+++ src/main/java/ru/teamandersen/AndersenTeamJavaApplication.java
@@ -2,12 +2,18 @@ package ru.teamandersen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.teamandersen.entity.Student;
+import ru.teamandersen.service.WorkWithExel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootApplication
 public class AndersenTeamJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AndersenTeamJavaApplication.class, args);
+
     }
 
 }
