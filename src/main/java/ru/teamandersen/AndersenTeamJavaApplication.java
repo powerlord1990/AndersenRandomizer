6b8b1c9cb4a19fe6032c28891034e9baf434564b
Alexey <unblocksteam@gmail.com>
2022-01-27 23:31:19 +0300

    Update AndersenTeamJavaApplication.java

--- src/main/java/ru/teamandersen/AndersenTeamJavaApplication.java
+++ src/main/java/ru/teamandersen/AndersenTeamJavaApplication.java
@@ -11,9 +11,7 @@ import java.util.List;
 
 @SpringBootApplication
 public class AndersenTeamJavaApplication {
-
     public static void main(String[] args) {
-
+        SpringApplication.run(AndersenTeamJavaApplication.class, args);
     }
-
 }
